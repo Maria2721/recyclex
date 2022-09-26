467d3a7064175327a53202819a82a87647edf9a8
AlisaCosmos <57442445+AlisaCosmos@users.noreply.github.com>
2022-09-26 18:01:24 +0300

    Доработан адаптив блока

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,4 +1,5 @@
 import FirstScreen from '../../components/FirstScreen/FirstScreen';
+import Advantaget from '../../components/Advantages/Advantages';
 
 import "./MainPage.scss";
 
@@ -6,6 +7,7 @@ export default function MainPage() {
   return (
       <div className="container__row">
         <FirstScreen/>
+        <Advantaget/>
       </div>
   );
 }
