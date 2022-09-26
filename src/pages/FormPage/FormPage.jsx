bd4cef381c5d24dc1a2f49cb6e5e9fd1ad17c957
mrglkExim <m.galko@eximpartners.online>
2022-09-27 00:33:36 +0300

    Added components for radio and checkbox

--- src/pages/FormPage/FormPage.jsx
+++ src/pages/FormPage/FormPage.jsx
@@ -1,11 +1,45 @@
 import "./FormPage.scss";
+import FormMessage from "../../components/FormMessage/FormMessage";
+import FormAnswer from "../../components/FormAnswer/FormAnswer";
+import { questions } from "./questions";
+import Checkbox from "../../components/Checkbox/Checkbox";
+import RadioButton from "../../components/RadioButton/RadioButton";
+
+export default function FormPage() {
+    return (
+        <div className="form container__row">
+            <div className="form__inner">
+                <h1 className="form__header">Шаг 1</h1>
+                <FormMessage>С какими группами отходов Вы работаете? </FormMessage>
+                <div className="form__answer">
+                    <FormAnswer>
+                       {
+                          questions[0].options.map((item) => (
+                               <Checkbox key={item.id} value={item.value} id={item.id}/>
+                           ))
+                       }
+                    </FormAnswer>
+                    <FormAnswer>
+                    {
+                          questions[1].options.map((item) => (
+                               <RadioButton key={item.id} value={item.value} id={item.id} name={item.name}/>
+                           ))
+                       }
+                    </FormAnswer>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
 
 // массив с объектами, в которых вопрос, тип ответа, вариант ответа и порядковый номер (шаг)
 
-// первый – селект с выбором своего ответа (множественный выбор ответов или нет?) еще узнать про карту на странице контактов
+// первый – селект с выбором своего ответа (множественный выбор ответов или нет?)
 // второй - то же самое
-// третий - форма с заполнением данных (что при наведении на форму?
-//Фамилия Имя Отчество Организация Телефон Email
+// третий - форма с заполнением данных
+// Фамилия Имя Отчество Организация Телефон Email
 // четвертый – селект
 
 // На каждом шаге сохраняем инфу и записываем в ответ, потом отправляем на бэк (пока что в консоль)
@@ -14,9 +48,3 @@ import "./FormPage.scss";
 // баблы сделать пока просто рамкой, потом прихерачить туда жопку
 
 // рисуем сразу все шаги, по состоянию переключаем их. По клику на кнопки переходим в следующее состояние
-export default function FormPage() {
-    return (
-        <div className="container__row">
-        </div>
-    );
-}
