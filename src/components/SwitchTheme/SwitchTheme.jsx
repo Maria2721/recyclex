495e1c10ce10819fc72ff0164cd0ba051cb15e08
mrglkExim <m.galko@eximpartners.online>
2022-10-02 20:22:06 +0200

    Delete box-shadow from switch

--- src/components/SwitchTheme/SwitchTheme.jsx
+++ src/components/SwitchTheme/SwitchTheme.jsx
@@ -9,8 +9,6 @@ export default function SwitchTheme({toggleTheme, theme}) {
       onHandleColor="#ffffff"
       uncheckedIcon={false}
       checkedIcon={false}
-      boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-      activeBoxShadow="0px 0px 1px 5px rgba(0, 0, 0, 0.2)"
       className="react-switch"
       id="material-switch"/>
       <span className="switch__label">{theme === 'light' ? "Light mode" : "Dark mode"}</span>
