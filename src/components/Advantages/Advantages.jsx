c3885a7fb6a2c0c61cf085689a5eb803efd7f495
AlisaDmitrenko <a.dmitrenko@eximpartners.online>
2022-10-28 20:22:49 +0300

    Добавила адаптив 4к в блоке advantages

--- src/components/Advantages/Advantages.jsx
+++ src/components/Advantages/Advantages.jsx
@@ -14,38 +14,38 @@ export default function Advantages() {
                 <h1 className="advantages__title">Здесь все необходимое</h1>
                 <div className="advantages__items">
                     <div className="advantages__item">
-                        <div className="item__img">
+                        <div className="advantages__item_row item__img">
                             <img src={Partrers}  width="60px" alt="img partrers"/>
                         </div>
-                        <div className="item__wrapper">
+                        <div className="advantages__item_row item__wrapper">
                             <h4 className="item__title">База партнеров</h4>
                             <p className="item__text">Удобный бизнес каталог</p>
                         </div>
                     </div>
                     <div className="advantages__item">
-                        <div className="item__img">
+                        <div className="item__img advantages__item_row">
                             <img src={Money}  width="60px" alt="img money"/>
                         </div>
-                        <div className="item__wrapper">
+                        <div className="advantages__item_row item__wrapper">
                             <h4 className="item__title">Лучшие цены</h4>
                             <p className="item__text">Анализ актуальных цен на вторсырье</p> 
                         </div>
                         
                     </div>
                     <div className="advantages__item">
-                        <div className="item__img">
+                        <div className="advantages__item_row item__img">
                             <img src={Deals} width="60px"  alt="img deals"/>
                         </div>
-                        <div className="item__wrapper">
+                        <div className="advantages__item_row item__wrapper">
                             <h4 className="item__title">Безопасные сделки</h4>
                             <p className="item__text">Удобный бизнес-каталог</p>
                         </div>
                     </div>
                     <div className="advantages__item">
-                        <div className="item__img">
+                        <div className="advantages__item_row item__img">
                             <img src={Community} width="60px"  alt="img community"/>
                         </div>
-                        <div className="item__wrapper">
+                        <div className="advantages__item_row item__wrapper">
                             <h4 className="item__title">Сообщество</h4>
                             <p className="item__text">Предприятий, организаций и заинтересованных лиц на одной площадке</p>
                         </div>
