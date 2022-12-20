0dabf69e217bbc76b6905596703553843468bc22
mrglkExim <m.galko@eximpartners.online>
2022-12-20 14:10:51 +0100

    Save to session data from main form

--- src/components/SwitchTheme/SwitchTheme.jsx
+++ src/components/SwitchTheme/SwitchTheme.jsx
@@ -3,7 +3,7 @@ import ReactSwitch from "react-switch";
 
 export default function SwitchTheme({toggleTheme, theme, tabindex}) {
   return (
-    <div className="switch" tabindex={tabindex} onKeyDown={(e) => e.key === "Enter" && toggleTheme()}>
+    <div className="switch" tabIndex={tabindex} onKeyDown={(e) => e.key === "Enter" && toggleTheme()}>
       <ReactSwitch onChange={toggleTheme} checked={theme === 'dark'}
       onColor="#000000"
       onHandleColor="#ffffff"
