abd25cc461f10a73ef02e22cb450f4216a398e46
Maria2721 <mashaivvas@gmail.com>
2023-12-08 21:35:06 +0300

    fixed a routing for deployment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import "./polyfill";
 import "./styles/common.scss";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router} from 'react-router-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/recyclex">
       <App />
     </Router>
   </React.StrictMode>
